1e2e4c97308aef49cd2e44df9a12ce738bb5bdcd
shoaibak98 <53967884+shoaibak98@users.noreply.github.com>
2019-08-10 15:25:23 +0530

    Update swiss1.c

--- swiss1.c
+++ swiss1.c
@@ -1,22 +1,9 @@
-#include stdio.h
-#include stdlib.h
-void mainh()
+int free(x,p)
 {
-int a;
-float d;
-println("Enter the amount:");
-scanf("%d",&a);
-println("Enter the Discount%:");
-scanf("%f",&d)
-printf("%d,",a);
-while(a>=0)
+while(x>0)
 {
-a=((100-a)/100)*a;
-printf("%d,",a);
+x=((100-p)/100)*x;
 }
-if(a<0)
-{
-printf("FREE!!");
-}
+printf("%d",x);
 return 0;
 }
